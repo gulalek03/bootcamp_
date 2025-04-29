25859d533f8bdfb962f4790f629a681a16995615
html.motivationalposter <nuryagdyeva03@gmail.com>
2025-04-29 09:33:52 +0300

    Add files via upload

---
+++ 23. Node/2.1 Using Node/index.js
@@ -0,0 +1,10 @@
+console.log("helloooo");
+
+console.log("blaknot");
+
+
+function birzat() {
+    var name = Gulalek;
+    var age = 21;
+ console.log( name + " and " + age) ;
+}
---
+++ 23. Node/2.3 NPM/index.js
@@ -0,0 +1,13 @@
+
+// bu Angelan yylyndaky kod
+// import superheroes from "superheroes";
+
+// const name = superheroes.random();
+// console.log("I am ${name}.");
+
+
+import {randomSuperhero} from 'superheroes';
+
+const x = randomSuperhero();
+
+console.log(x);
